65faf8a0867dabf68c15b6b3a7c5afb31a256c56
Brian Carcich <brian.carcich@kinetx.com>
2019-07-10 10:29:33 -0400

    Add comments; modify plot slightly

--- plot12rolls.py
+++ plot12rolls.py
@@ -7,32 +7,36 @@ if "__main__" == __name__:
 
   expected = 794
 
+  ### Convert flat file to floats
   lt_data = list(map(float,sys.stdin))
+
+  ### Number of cases
   count = len(lt_data)
+
+  ### Extent
   mn,mx = map(int,[min(lt_data),max(lt_data)+2])
+
+  ### Bins to plot
   bins = range(mn,mx)
 
+  ### Calculate average
   avg = sum(lt_data)/count
-  plt.xlim([mn,mx])
-  plt.axvline(expected,color='k',label='Expected ({0})'.format(expected))
-  plt.axvline(avg,color='r',label='Mean')
-  plt.hist(lt_data,bins=bins,histtype='bar',label='Histogram')
-  plt.xlabel('Modeled cases per 4096 rolls with even count<5')
-  plt.ylabel('Count of occurences of each value of cases')
-  plt.title('{0} cases; mean={1:.2f}'.format(count,avg))
-  plt.legend()
-  plt.show()
 
-"""
-  plt.xlim([1,i])
-  plt.axhline(0.)
-  plt.axhline(-1.)
-  plt.axhline(-2.)
-  plt.axhline(-3.)
-  plt.plot(ipos,dpos,'b.',label='N>{0}'.format(expected))
-  plt.plot(ineg,dneg,'r.',label='N<{0}'.format(expected))
-  plt.ylabel('log10(|N-{0}|); N=running avg cases/4096 w/even count<5'.format(expected))
-  plt.xlabel('4096-sample run')
+  ### Set abscissa limits
+  plt.xlim([mn,mx])
+
+  ### Plot vertical lines at expected value and at mean
+  plt.axvline(expected+0.5,color='k',label='Expected ({0})'.format(expected))
+  plt.axvline(avg,color='r',label='Mean')
+
+  ### Plot histogram
+  plt.hist(lt_data,bins=bins,histtype='bar',label='Histogram')
+
+  ### Annotate plot
+  plt.xlabel('Modeled cases per 4096 12-roll runs with even count<5')
+  plt.ylabel('Count of occurences of each value of cases')
+  plt.title('{0} cases; 4096 12-roll runs per case; mean={1:.2f}'.format(count,avg))
   plt.legend()
+
+  ### Show plot
   plt.show()
-"""
--- twelverolls.py
+++ twelverolls.py
@@ -1,28 +1,60 @@
 """
 Usage:
 
+  python twelverolls.py [--loops=41] [--shift=4]
+
+Sample usage:
+
   python twelverolls.py --loops=1000 | python plot12rolls.py 
+
+Modeling:
+
+- Use /dev/urandom to get random bytes
+- Extract bit 4 (1<<4 => 16) as indication of even or odd roll of die
 
 """
 import os
 import sys
 if "__main__" == __name__:
-  n_loops = 41.0
+
+  ### Defaults
+  n_loops = 41
   bit_shift = 4
+
+  ### Process input arguments
   for arg in sys.argv[1:]:
     if arg.startswith('--loops='): n_loops = int(arg.split('=')[1])
     elif arg.startswith('--shift='): bit_shift = int(arg.split('=')[1])
+    else:
+      sys.stderr.write('WARNING:  invalid argument [{0}] ignored\n'.format(arg))
+
+  ### Convert shift to bit
   i_bit = 1<<bit_shift
-  n_794 = 0
-  i_loop = 0.
+
+  ### Open uniform random bytes file
   with open("/dev/urandom","rb") as f_random:
-    while i_loop < n_loops:
-      i_loop += 1.0
+
+    ### Start sample loop; each loop models 4096 12-roll runs
+    for i_loop in range(n_loops):
+
+      ### Initialize counter
       n = 0
+
+      ### Perform 4096 12-roll runs
       for roll in range(4096):
+
+        ### Model 12 rolls via 12 bytes from f_random (urandom file)
+        ### - Bit i_bit of each byte models even roll if set, else odd
+        ### - List of conditional None's models resulting even rolls
+        ### - Increment counter if less than five even rolls
         n += (5 > len([None
-                       for c in f_random.read(12).decode('8859')
-                       if (ord(c) & i_bit)
+                       ### Convert binary to string, loop over chars
+                       for char in f_random.read(12).decode('8859')
+                       ### ord() converts char to inty; test bit i_t
+                       if (ord(char) & i_bit)
+                      ### Add 1 to counter if bit is set (=> even roll)
                       ])) and 1 or 0
+
+      ### Print number of 12-roll runs with less than 5 even rolls
       print(n)
       sys.stdout.flush()
